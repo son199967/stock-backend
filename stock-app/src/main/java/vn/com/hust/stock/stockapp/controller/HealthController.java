4f17883f0e1cb73def354de71632d3408982128d
Son28071968 <nguyentrungson31899@gmail.com>
2021-09-17 10:51:38 +0700

    update code

--- stock-app/src/main/java/vn/com/hust/stock/stockapp/controller/HealthController.java
+++ stock-app/src/main/java/vn/com/hust/stock/stockapp/controller/HealthController.java
@@ -1,13 +1,17 @@
 package vn.com.hust.stock.stockapp.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/health")
 public class HealthController {
+
+    @Value("${abc}")
+    private String abc;
     @RequestMapping
     public String healthCheck(){
-        return "Ready  App ";
+        return "Ready  App "+abc;
     }
 }
