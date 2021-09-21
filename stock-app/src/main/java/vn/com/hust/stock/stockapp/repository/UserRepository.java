bea44354ce7bfcb03d6802419f7dfa9627309f5d
Son28071968 <nguyentrungson31899@gmail.com>
2021-09-21 14:58:15 +0700

    add

--- stock-app/src/main/java/vn/com/hust/stock/stockapp/repository/UserRepository.java
+++ stock-app/src/main/java/vn/com/hust/stock/stockapp/repository/UserRepository.java
@@ -8,7 +8,7 @@ import vn.com.hust.stock.stockmodel.user.User;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Long> {
-    Optional<User> findByUserName(String username);
-    Boolean existsByUsername(String username);
-    Boolean existsByEmail(String email);
+//    Optional<User> findByUserName(String username);
+//    Boolean existsByUsername(String username);
+//    Boolean existsByEmail(String email);
 }
